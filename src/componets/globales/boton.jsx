a8d3643c7fa16bd010f3fb603f4fe0dc7b643e62
Lopez Ariel <saltaportal210@gmail.com>
2023-07-07 19:36:45 -0300

    proyecto frontend de backend

---
+++ src/componets/globales/boton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Boton= ({titulo})=>{
+    
+    return (
+        <div className="frame">
+          <button className="custom-btn btn-16">{titulo}</button>
+        </div>
+    )
+}
+export default Boton;
